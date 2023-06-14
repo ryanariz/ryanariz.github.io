6e9426d6706b0fd57a8a5402dfef3dbf1240afa1
ryanariz <136644724+ryanariz@users.noreply.github.com>
2023-06-14 16:38:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function pageFunc(id) {
+	const textToReplace = document.querySelector(".replace");
+	const newText = document.getElementById(id);
+	textToReplace.innerHTML = newText.innerHTML;
+	const page = document.querySelector(".page");
+	page.classList.remove("hidden");
+	newText.classList.add("hidden");
+}
+
+function menuChange(x) {
+	x.classList.toggle("change");
+	const menu = document.querySelector(".mobile-menu");
+	if (x.classList.contains("change")) {
+		menu.classList.add("change");
+	} else {
+		menu.classList.remove("change");
+	}
+}
+
+function itemClicked() {
+	const menu = document.querySelector(".mobile-menu");
+	const burger = document.querySelector(".hamburger")
+	if (menu.classList.contains("change")){
+		menu.classList.remove("change");
+		burger.classList.remove("change");
+	}
+}
+
